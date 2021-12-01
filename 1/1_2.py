5bff1fd510d01cb4a0f54bf4f9206911cea33283
proman3419 <proman7268@gmail.com>
2021-12-01 19:15:34 +0100

    Simplified 1_2

--- 1/1_2.py
+++ 1/1_2.py
@@ -1,11 +1,4 @@
 with open('1_2i.txt') as f:
     A = list(map(int, f.readlines()))
 
-n = len(A)
-_sum = 0
-for i in range(n-3):
-    three_meas = sum(A[i+j] for j in range(3))
-    if three_meas < three_meas - A[i] + A[i+3]:
-        _sum += 1        
-
-print(_sum)
+print(sum(A[i] < A[i+3] for i in range(len(A)-3)))
