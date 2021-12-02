167c642408d13428979d49c0a7d2e1297004e76f
proman3419 <proman7268@gmail.com>
2021-12-02 18:19:41 +0100

    Removed the duplicate input file

--- 1/1_1.py
+++ 1/1_1.py
@@ -1,4 +1,4 @@
-with open('1_1i.txt') as f:
+with open('1_i.txt') as f:
     A = list(map(int, f.readlines()))
 
 print(sum(A[i] < A[i+1] for i in range(len(A)-1)))
--- 1/1_2.py
+++ 1/1_2.py
@@ -1,4 +1,4 @@
-with open('1_2i.txt') as f:
+with open('1_i.txt') as f:
     A = list(map(int, f.readlines()))
 
 print(sum(A[i] < A[i+3] for i in range(len(A)-3)))
